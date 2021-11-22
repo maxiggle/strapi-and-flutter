6dfda65ffc899ef347c2a5b2797fbee8bf81ab8d
Godwin Alexander Ekainu <clsfriday@gmail.com>
2021-11-22 17:47:52 +0100

    strapi and flutter

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f8816cb730b71eb649a1eea9cdd6b8ed'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4eef4883-12ac-428f-99a1-5ff209a57f69'
+};
